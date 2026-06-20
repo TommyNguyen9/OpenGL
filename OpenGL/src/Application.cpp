7d6ba88a0deeca6fede255f778edd02eb6b5a538
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-20 20:56:56 +0100

    Debugged linker

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,6 +1,38 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
-int main()
+int main(void)
 {
-	std::cout << "Hello" << std::endl;
+	GLFWwindow* window;
+
+	/* Initialize library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create windowed mode window & its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make window's context current*/
+	glfwMakeContextCurrent(window);
+
+	/* Loop until user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front & back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for & process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+
 }
