7a2e8f654d827f0742ddf520c126c768957a32d5
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-23 17:31:15 +0100

    Party mode rectangle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -128,6 +128,8 @@ int main(void)
 	/* Make window's context current*/
 	glfwMakeContextCurrent(window);
 
+	glfwSwapInterval(1);
+
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error!" << std::endl;
 
@@ -167,7 +169,10 @@ int main(void)
 
 	GLCall(int location = glGetUniformLocation(shader, "u_Color"));
 	ASSERT(location != -1);
-	GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+	GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+	float r = 0.0f;
+	float increment = 0.05f;
 
 	/* Loop until user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -175,8 +180,15 @@ int main(void)
 		/* Render here */
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
-	
+		
+		if (r > 1.0f)
+			increment = -0.05f;
+		else if (r < 0.0f)
+			increment = 0.05f;
+
+		r += increment;
 		
 		/* Swap front & back buffers */
 		glfwSwapBuffers(window);
