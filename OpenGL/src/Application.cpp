9d7f2fee1dc9d03e2c36378e1606f71741a1eb79
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-23 19:09:21 +0100

    Added vertexbuffer & indexbuffer files

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -6,27 +6,7 @@
 #include <string>
 #include <sstream>
 
-#define ASSERT(x) if (!(x)) __debugbreak();
-#define GLCall(x) GLClearError();\
-	x;\
-	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
-
-static void GLClearError()
-{
-	while (glGetError() != GL_NO_ERROR);
-}
-
-static bool GLLogCall(const char* function, const char* file, int line)
-{
-	while (GLenum error = glGetError())
-	{
-		std::cout << "[OPENGL Error] (" << error << "): " << function <<
-			" " << file << ":" << line << std::endl;
-		return false;
-	}
-	return true;
-}
-
+#include "Renderer.h"
 
 struct ShaderProgramSource
 {
