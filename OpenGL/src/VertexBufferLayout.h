8e640176ebcbf9881c61df5eb4db54e2a354778b
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-24 20:05:50 +0100

    Vertex array cpp & header files created

--- OpenGL/src/VertexBufferLayout.h
+++ OpenGL/src/VertexBufferLayout.h
@@ -17,8 +17,10 @@ class VertexBufferLayout
 {
 private:
 	std::vector<VertexBufferElement> m_Elements;
+	unsigned int m_Stride;
 public:
-	VertexBufferLayout();
+	VertexBufferLayout()
+		: m_Stride(0) {}
 
 	template<typename T>
 	void Push(unsigned int count)
@@ -30,17 +32,23 @@ public:
 	void Push<float>(unsigned int count)
 	{
 		m_Elements.push_back(VertexBufferElement{ GL_FLOAT, count, false });
+		m_Stride += sizeof(GLfloat);
 	}
 
 	template<>
 	void Push<unsigned int>(unsigned int count)
 	{
 		m_Elements.push_back(VertexBufferElement{ GL_UNSIGNED_INT, count, false });
+		m_Stride += sizeof(GLuint);
 	}
 
 	template<>
 	void Push<unsigned char>(unsigned int count)
 	{
 		m_Elements.push_back(VertexBufferElement{ GL_UNSIGNED_BYTE, count, true });
+		m_Stride += sizeof(GLbyte);
 	}
+
+	inline const std::vector<VertexBufferElement>& GetElements() const { return m_Elements; }
+	inline unsigned int GetStride() const { return m_Stride; }
 };
