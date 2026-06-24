905889f14aa3792bdda881026505a1ea7e5dc326
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-24 20:55:43 +0100

    Buffer layout abstraction

--- OpenGL/src/VertexBufferLayout.h
+++ OpenGL/src/VertexBufferLayout.h
@@ -32,7 +32,8 @@ private:
 	unsigned int m_Stride;
 public:
 	VertexBufferLayout()
-		: m_Stride(0) {}
+		: m_Stride(0) {
+	}
 
 	template<typename T>
 	void Push(unsigned int count)
@@ -40,27 +41,29 @@ public:
 		static_assert(false);
 	}
 
+	inline const std::vector<VertexBufferElement>& GetElements() const { return m_Elements; }
+	inline unsigned int GetStride() const { return m_Stride; }
+};
+
 	template<>
-	void Push<float>(unsigned int count)
+	inline void VertexBufferLayout::Push<float>(unsigned int count)
 	{
 		m_Elements.push_back(VertexBufferElement{ GL_FLOAT, count, GL_FALSE });
 		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_FLOAT);
 	}
 
 	template<>
-	void Push<unsigned int>(unsigned int count)
+	inline void VertexBufferLayout::Push<unsigned int>(unsigned int count)
 	{
 		m_Elements.push_back(VertexBufferElement{ GL_UNSIGNED_INT, count, GL_FALSE });
 		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT);
 	}
 
 	template<>
-	void Push<unsigned char>(unsigned int count)
+	inline void VertexBufferLayout::Push<unsigned char>(unsigned int count)
 	{
 		m_Elements.push_back(VertexBufferElement{ GL_UNSIGNED_BYTE, count, GL_TRUE });
 		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);
 	}
 
-	inline const std::vector<VertexBufferElement>& GetElements() const { return m_Elements; }
-	inline unsigned int GetStride() const { return m_Stride; }
-};
+	
