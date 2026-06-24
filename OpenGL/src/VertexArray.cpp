9659f9cbd120ec8b426fcff01e961d2647d50c8d
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-24 20:25:11 +0100

    Offset variable

--- OpenGL/src/VertexArray.cpp
+++ OpenGL/src/VertexArray.cpp
@@ -17,12 +17,13 @@ void VertexArray::AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& la
 {
 	vb.Bind();
 	const auto& elements = layout.GetElements();
+	unsigned int offset = 0;
 	for (unsigned int i = 0; i < elements.size(); i++)
 	{
 		const auto& element = elements[i];
 		GLCall(glEnableVertexAttribArray(i));
 		GLCall(glVertexAttribPointer(i, element.count, element.type, 
-			element.normalized ? , sizeof(float) * 2, 0));
+			element.normalized, layout.GetStride(), offset));
 	}
 	
 }
