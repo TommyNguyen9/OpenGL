d00fa9dfc1acbc3b14fba7743556da89a3b6bde2
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-27 16:50:06 +0100

    Refactoring using clear function

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -11,6 +11,7 @@
 #include "indexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "VertexBufferLayout.h"
 
 int main(void)
 {
@@ -80,6 +81,8 @@ int main(void)
 		ib.Unbind();
 		shader.Unbind();
 
+		Renderer renderer;
+
 		float r = 0.0f;
 		float increment = 0.05f;
 
@@ -87,16 +90,13 @@ int main(void)
 		while (!glfwWindowShouldClose(window))
 		{
 			/* Render here */
-			GLCall(glClear(GL_COLOR_BUFFER_BIT));
+			renderer.Clear();
 
 			shader.Bind();
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 		
-			va.Bind();
-			ib.Bind();
-
-			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
-
+			renderer.Draw(va, ib, shader);
+			
 			if (r > 1.0f)
 				increment = -0.05f;
 			else if (r < 0.0f)
--- OpenGL/src/Renderer.h
+++ OpenGL/src/Renderer.h
@@ -3,6 +3,9 @@
 #include <GL/glew.h>
 #include <iostream>
 
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
 
 #define ASSERT(x) if (!(x)) __debugbreak();
 #define GLCall(x) GLClearError();\
@@ -12,3 +15,11 @@
 
 void GLClearError();
 bool GLLogCall(const char* function, const char* file, int line);
+
+class Renderer 
+{
+public:
+	void Clear() const;
+	void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+	
+};
