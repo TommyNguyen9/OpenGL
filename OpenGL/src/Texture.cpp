44c1513974376661a02121ad6383dfc289d4ebc6
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-27 20:45:27 +0100

    Initializing the texture

--- OpenGL/src/Texture.cpp
+++ OpenGL/src/Texture.cpp
@@ -1,9 +1,11 @@
 #include "Texture.h"
 
 Texture::Texture(const std::string& path)
-	: m_RendererID(0), m_FilePath(path), m_LocalBuffer(nullptr), m_Width(0), m_Height(0), m_BPP(0)
+	: m_RendererID(0), m_FilePath(path), m_LocalBuffer(nullptr), 
+	m_Width(0), m_Height(0), m_BPP(0)
 {
-
+	GLCall(glGenTextures(1, &m_RendererID));
+	GLCall(glBindTexture(GL_TEXTURE_2D, m_RendererID));
 }
 
 Texture::~Texture()
