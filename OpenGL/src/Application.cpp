b8eb1b64c46e3514a8516c31cc00b7190ff0bf5e
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-28 18:19:08 +0100

    Basic shader update

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -12,6 +12,7 @@
 #include "VertexArray.h"
 #include "Shader.h"
 #include "VertexBufferLayout.h"
+#include "Texture.h"
 
 int main(void)
 {
@@ -46,10 +47,10 @@ int main(void)
 	{
 
 		float positions[] = {
-			-0.5f, -0.5f, // 0
-			 0.5f, -0.5f, // 1
-			 0.5f,  0.5f, // 2
-			-0.5f,  0.5f, // 3
+			-0.5f, -0.5f, 0.0f, 0.0f, // 0
+			 0.5f, -0.5f, 1.0f, 0.0f,// 1
+			 0.5f,  0.5f, 1.0f, 1.0f,// 2
+			-0.5f,  0.5f, 0.0f, 1.0f// 3
 
 		};
 
@@ -63,9 +64,10 @@ int main(void)
 		GLCall(glBindVertexArray(vao));
 
 		VertexArray va;
-		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
 		VertexBufferLayout layout;
+		layout.Push<float>(2);
 		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 
@@ -75,6 +77,10 @@ int main(void)
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+		Texture texture("res/textures/Cherno.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
 
 		va.Unbind();
 		vb.Unbind();
