e919e6642f032a49da628f3ee0dfa6305a7b7fc3
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-07-03 21:19:32 +0100

    Code refactoring for texture tests

--- OpenGL/src/tests/TestTexture2D.cpp
+++ OpenGL/src/tests/TestTexture2D.cpp
@@ -2,17 +2,19 @@
 
 #include "Renderer.h"
 #include "imgui/imgui.h"
-#include "VertexBuffer.h"
-#include "VertexBufferLayout.h"
+
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
-#include "Texture.h"
+
+#include <fstream>
 
 namespace test {
 
 	TestTexture2D::TestTexture2D()
+		: m_TranslationA(200, 200, 0), m_TranslationB(400, 200, 0)
 		
 	{
+	
 		float positions[] = {
 			-50.0f, -50.0f, 0.0f, 0.0f, // 0
 			50.5f, -50.0f, 1.0f, 0.0f,// 1
@@ -29,42 +31,23 @@ namespace test {
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 		GLCall(glEnable(GL_BLEND));
 
-		VertexArray va;
+		m_Shader = std::make_unique<Shader>("res/shaders/Basic.shader");
+		m_VAO = std::make_unique<VertexArray>();
 		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-	
-
 		VertexBufferLayout layout;
 		layout.Push<float>(2);
 		layout.Push<float>(2);
-		va.AddBuffer(vb, layout);
+
+		m_VAO->AddBuffer(vb, layout);
+		m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 6);
+
+		m_Proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		m_View = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+		m_Shader->Bind();
+		m_Shader->SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+		m_Texture = std::make_unique<Texture>("../res/textures/Cherno.png");
+		m_Shader->SetUniform1i("u_Texture", 0);
 	
-
-		IndexBuffer ib(indices, 6);
-
-
-		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-	
-
-		Shader shader("res/shaders/Basic.shader");
-		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-
-
-		std::cout << "Current path: " << std::filesystem::current_path() << std::endl;
-	
-
-		Texture texture("../res/textures/Cherno.png");
-		texture.Bind();
-		shader.SetUniform1i("u_Texture", 0);
-	
-		va.Unbind();
-		vb.Unbind();
-		ib.Unbind();
-		shader.Unbind();
-
-
 	}
 
 	TestTexture2D::~TestTexture2D()
@@ -82,31 +65,34 @@ namespace test {
 		GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-		{
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-			glm::mat4 mvp = proj * view * model;
-			shader.Bind();
-			shader.SetUniformMat4f("u_MVP", mvp);
+		Renderer renderer;
 
-			renderer.Draw(va, ib, shader);
+		m_Texture->Bind();
+
+		{
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), m_TranslationA);
+			glm::mat4 mvp = m_Proj * m_View * model;
+			m_Shader->Bind();
+			m_Shader->SetUniformMat4f("u_MVP", mvp);
+			renderer.Draw(*m_VAO, *m_IndexBuffer, *m_Shader);
 
 		}
 
 		{
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-			glm::mat4 mvp = proj * view * model;
-			shader.Bind();
-			shader.SetUniformMat4f("u_MVP", mvp);
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), m_TranslationB);
+			glm::mat4 mvp = m_Proj * m_View * model;
+			m_Shader->Bind();
+			m_Shader->SetUniformMat4f("u_MVP", mvp);
 
-			renderer.Draw(va, ib, shader);
+			renderer.Draw(*m_VAO, *m_IndexBuffer, *m_Shader);
 
 		}
 	}
 
 	void TestTexture2D::OnImGuiRender()
 	{
-		ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-		ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+		ImGui::SliderFloat3("Translation A", &m_TranslationA.x, 0.0f, 960.0f);
+		ImGui::SliderFloat3("Translation B", &m_TranslationB.x, 0.0f, 960.0f);
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 	}
 }
